1d9e4008e3942e88dc1c014b2d0cdc2dccc9dc9e
shelialia <teo.zihui.shelia@gmail.com>
2025-01-18 15:33:07 +0800

    Level 0: Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Rose User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Rose.java
@@ -0,0 +1,11 @@
+public class Rose {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Rose");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
