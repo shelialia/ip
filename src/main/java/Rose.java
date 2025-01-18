81e61fbe27b2cac6319e3a8e7c19a586d5dc22c4
shelialia <teo.zihui.shelia@gmail.com>
2025-01-18 17:03:53 +0800

    Level 1: Echo

--- src/main/java/Rose.java
+++ src/main/java/Rose.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Rose {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
@@ -5,7 +6,24 @@ public class Rose {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                // Exit message
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            // Echo input back to the user
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+        scanner.close();
     }
 }
