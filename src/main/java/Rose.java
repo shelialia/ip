b06640cf7189d3ac50338f93b531e82ac31db5f2
shelialia <teo.zihui.shelia@gmail.com>
2025-01-18 17:17:16 +0800

    Level 2: Add and List

--- src/main/java/Rose.java
+++ src/main/java/Rose.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Rose {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
@@ -6,11 +8,12 @@ public class Rose {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
-        String input;
 
         while (true) {
-            input = scanner.nextLine();
+            String input = scanner.nextLine();
 
             if (input.equalsIgnoreCase("bye")) {
                 // Exit message
@@ -18,11 +21,23 @@ public class Rose {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                if (taskCount == 0) {
+                    System.out.println("You have not added any tasks!");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ". " +  tasks[i]);
+                    }
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
-            // Echo input back to the user
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
         scanner.close();
     }
