aefe115b34405209e67d2b67561b271d5b96318d
shelialia <teo.zihui.shelia@gmail.com>
2025-01-18 18:19:05 +0800

    Level 3: Edited chatbot logic to use written classes

--- src/main/java/Rose.java
+++ src/main/java/Rose.java
@@ -55,8 +55,42 @@ public class Rose {
                 } catch (Exception e) {
                     System.out.println(" Invalid task number!");
                 }
-            }
-            else {
+            } else if (input.startsWith("todo ")) {
+                String description = input.substring(5).trim();
+                tasks[taskCount] = new Todo(description);
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[taskCount - 1]);
+                System.out.println(" Now you have " + taskCount + " totasks in the list.");
+                System.out.println("____________________________________________________________");
+            } else if (input.startsWith("deadline ")) {
+                String[] temp = input.substring(9).split(" /by ", 2);
+                if (temp.length < 2) {
+                    System.out.println(" Invalid deadline format.");
+                    continue;
+                }
+                tasks[taskCount] = new Deadline(temp[0].trim(), temp[1].trim());
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[taskCount - 1]);
+                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+            } else if (input.startsWith("event ")) {
+                String[] temp = input.substring(6).split(" /from | /to ", 3);
+                if (temp.length < 3) {
+                    System.out.println(" Invalid event format! Use: event <description> /from <start> /to <end>");
+                    continue;
+                }
+                tasks[taskCount] = new Event(temp[0].trim(), temp[1].trim(), temp[2].trim());
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println("   " + tasks[taskCount - 1]);
+                System.out.println(" Now you have " + taskCount + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+            } else {
                 tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("____________________________________________________________");
