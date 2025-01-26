719552bd8eb5c1b0a2c0a135c58c539d19bb77e6
shelialia <teo.zihui.shelia@gmail.com>
2025-01-26 11:59:22 +0800

    Implement Storage class for saving and loading tasks. Update Task classes to support file formatting. Integrate Storage with Rose chatbot

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,10 +2,15 @@ public class Event extends Task {
     protected String from;
     protected String to;
 
-    public Event(String description, String from, String to) {
-        super(description, TaskType.EVENT);
+    public Event(String description, String from, String to, boolean isDone) {
+        super(description, TaskType.EVENT, isDone);
         this.from = from;
         this.to = to;
+    }
+
+    @Override
+    public String toFileFormat() {
+        return "E" + super.toFileFormatPrefix() + super.description + " | " + this.from + this.to;
     }
 
     @Override
