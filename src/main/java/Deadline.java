d7673d7d4905165a29706768150a6e72abaf57f3
shelialia <teo.zihui.shelia@gmail.com>
2025-01-26 14:53:29 +0800

    change LocalDate to LocalDateTime object in Deadline class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,14 +1,15 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task {
-    protected LocalDate by;
+    protected LocalDateTime by;
     private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
     private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mm a");
 
     public Deadline(String description, String by, boolean isDone) {
         super(description, TaskType.DEADLINE, isDone);
-        this.by = LocalDate.parse(by, INPUT_FORMAT);
+        this.by = LocalDateTime.parse(by, INPUT_FORMAT);
     }
 
     @Override
